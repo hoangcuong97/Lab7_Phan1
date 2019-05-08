82effab90a2139518678ded297d1676fdf06bca3
Hoang cuong <manhcuonghc97@gmail.com>
2019-05-08 22:25:07 +0700

    first commit

---
+++ MVC/Controllers/TeacherController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class TeacherController : Controller
+    {
+        private QLTrungTamEntities1 db = new QLTrungTamEntities1();
+
+        // GET: /Teacher/
+        public ActionResult Index()
+        {
+            var teachers = db.Teachers.Include(t => t.User);
+            return View(teachers.ToList());
+        }
+
+        // GET: /Teacher/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Teacher teacher = db.Teachers.Find(id);
+            if (teacher == null)
+            {
+                return HttpNotFound();
+            }
+            return View(teacher);
+        }
+
+        // GET: /Teacher/Create
+        public ActionResult Create()
+        {
+            ViewBag.UserID = new SelectList(db.Users, "UserID", "UserName");
+            return View();
+        }
+
+        // POST: /Teacher/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="TeacherID,TeacherName,UserID,Sex,Address,IdentityCard,Phone")] Teacher teacher)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Teachers.Add(teacher);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserID = new SelectList(db.Users, "UserID", "UserName", teacher.UserID);
+            return View(teacher);
+        }
+
+        // GET: /Teacher/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Teacher teacher = db.Teachers.Find(id);
+            if (teacher == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserID = new SelectList(db.Users, "UserID", "UserName", teacher.UserID);
+            return View(teacher);
+        }
+
+        // POST: /Teacher/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="TeacherID,TeacherName,UserID,Sex,Address,IdentityCard,Phone")] Teacher teacher)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(teacher).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserID = new SelectList(db.Users, "UserID", "UserName", teacher.UserID);
+            return View(teacher);
+        }
+
+        // GET: /Teacher/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Teacher teacher = db.Teachers.Find(id);
+            if (teacher == null)
+            {
+                return HttpNotFound();
+            }
+            return View(teacher);
+        }
+
+        // POST: /Teacher/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Teacher teacher = db.Teachers.Find(id);
+            db.Teachers.Remove(teacher);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
